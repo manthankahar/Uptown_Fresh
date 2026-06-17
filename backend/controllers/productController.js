73f5d29496127bd2acdadd1cf82566bcfcc6a8ba
manthan <manthankahar2006@gmail.com>
2026-06-17 19:36:40 +0530

    third commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,15 +1,26 @@
 const Product = require("../models/Product");
 
+
 // Get All Products
 const getProducts = async (req, res) => {
   try {
+
+    console.log("GET PRODUCTS HIT");
+
     const products = await Product.find();
 
-    res.status(200).json(products);
+    console.log(products);
+
+    res.json(products);
+
   } catch (error) {
+
+    console.log(error);
+
     res.status(500).json({
       message: error.message
     });
+
   }
 };
 
