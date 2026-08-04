7011d8c6f49082d2ccb044057ea9ae65f7d54149
manthan <manthankahar2006@gmail.com>
2026-08-04 08:43:18 +0530

    server.js ma change

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 const path = require("path");
 
 const connectDB = require("./config/db");
 
-dotenv.config();
+// dotenv.config();
 
 connectDB();
 
